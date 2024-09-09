81601ef4dccf4a59a3e2f7dc8c08eaeb8681aa6d
AtharvaParadkar <atharvaparadkar91@gmail.com>
2024-09-09 14:52:09 +0530

    Starting Project

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+//~ e9a99dba9ecbc9ba67dce9b36b508ab3
+
+import 'package:flutter/material.dart';
+
+void main()=>runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(brightness: Brightness.dark),
+      home: const Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter Movie App'),
+      ),
+    );
+  }
+}
