58fd66161fb627971fec716670a2ae7e5c0a32b3
AtharvaParadkar <atharvaparadkar91@gmail.com>
2024-09-09 16:04:39 +0530

    Fetching Data from API

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 //~ e9a99dba9ecbc9ba67dce9b36b508ab3
+//~ eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlOWE5OWRiYTllY2JjOWJhNjdkY2U5YjM2YjUwOGFiMyIsIm5iZiI6MTcyNTg3MzAwMi43MTU0ODYsInN1YiI6IjY2ZGViYTY3OGZjZWFkYWE4ZTNhZGI1MiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.PQRFUpo37BSqzK-u0p12dem4TolxzJQQWrMhga941eE
 
 import 'package:flutter/material.dart';
+import 'package:tmdb_api/tmdb_api.dart';
+import 'package:dio/dio.dart';
 
-void main()=>runApp(const MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -25,6 +28,39 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  List trendingMovies = [];
+  final String apiKey = 'e9a99dba9ecbc9ba67dce9b36b508ab3';
+  final readAccessToken =
+      'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlOWE5OWRiYTllY2JjOWJhNjdkY2U5YjM2YjUwOGFiMyIsIm5iZiI6MTcyNTg3MzAwMi43MTU0ODYsInN1YiI6IjY2ZGViYTY3OGZjZWFkYWE4ZTNhZGI1MiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.PQRFUpo37BSqzK-u0p12dem4TolxzJQQWrMhga941eE';
+
+  @override
+  void initState() {
+    loadmovies();
+    super.initState();
+  }
+
+  loadmovies() async {
+    TMDB tmdbWithCustomLogs = TMDB(
+      ApiKeys(apiKey, readAccessToken),
+    );
+    logConfig:
+    const ConfigLogger(
+      showLogs: true,
+      showErrorLogs: true,
+    );
+
+    try {
+      Map trendingResult = await tmdbWithCustomLogs.v3.trending.getTrending();
+      print(trendingResult);
+      setState(() {
+        trendingMovies=trendingResult['results'];
+      });
+      print(trendingMovies);
+    } catch (e) {
+      print('Error fetching movies: $e');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
