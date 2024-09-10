63c3e9b08dc0e32711246ed370d9c12aa42b8f10
AtharvaParadkar <atharvaparadkar91@gmail.com>
2024-09-10 12:25:22 +0530

    TV Shows Description

--- lib/widgets/tv.dart
+++ lib/widgets/tv.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tmdb_app/description.dart';
 import 'package:tmdb_app/utils/text.dart';
 
 class TV extends StatelessWidget {
@@ -21,34 +22,49 @@ class TV extends StatelessWidget {
               scrollDirection: Axis.horizontal,
               itemCount: tv.length,
               itemBuilder: (ctx, i) => InkWell(
-                onTap: () {},
-                child: Container(
-                  padding: const EdgeInsets.all(5),
-                  width: 250,
-                  child: Column(
-                    children: [
-                      Container(
-                        width: 250,
-                        height: 150,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20),
-                          image: DecorationImage(
-                            image: NetworkImage(
-                              'https://image.tmdb.org/t/p/w500${tv[i]['backdrop_path']}',
-                            ),
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                      ),
-                      const SizedBox(height: 10),
-                      MovieText(
-                        text: tv[i]['original_name'] ?? 'Loading',
-                        color: Colors.white,
-                        size: 16,
-                      )
-                    ],
+                onTap: () => Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (_) => Discription(
+                      name: tv[i]['original_name'],
+                      description: tv[i]['overview'],
+                      bannerurl:
+                          'https://image.tmdb.org/t/p/w500${tv[i]['backdrop_path']}',
+                      posterurl:
+                          'https://image.tmdb.org/t/p/w500${tv[i]['poster_path']}',
+                      rating: tv[i]['vote_average'].toString(),
+                      launchDate: tv[i]['first_air_date'],
+                    ),
                   ),
                 ),
+                child: tv[i]['original_name'] != null
+                    ? Container(
+                        padding: const EdgeInsets.all(5),
+                        width: 250,
+                        child: Column(
+                          children: [
+                            Container(
+                              width: 250,
+                              height: 150,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(20),
+                                image: DecorationImage(
+                                  image: NetworkImage(
+                                    'https://image.tmdb.org/t/p/w500${tv[i]['backdrop_path']}',
+                                  ),
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ),
+                            const SizedBox(height: 10),
+                            MovieText(
+                              text: tv[i]['original_name'] ?? 'Loading',
+                              color: Colors.white,
+                              size: 16,
+                            )
+                          ],
+                        ),
+                      )
+                    : Container(),
               ),
             ),
           ),
